0a998963efcd83ceff125caa9e9995fb04db8be4
leiv4 <nicolasleiva@proton.me>
2024-02-21 07:24:30 -0300

    Terminando ejercicio 26

--- js/ejercicios3.js
+++ js/ejercicios3.js
@@ -86,6 +86,35 @@ ejercicio25();
 // Hacer un programa que solicite el ingreso del importe total de la venta y la
 // cantidad de litros vendidos y calcule y emita el importe con el descuento
 // aplicado.
+
+function ejercicio26() {
+  let litrosEjercicio26 = parseInt(
+    prompt("Ejercicio 26: Ingresa la cantidad de litros vendidos")
+  );
+  let importeEjercicio26 = parseInt(
+    prompt("Ejercicio 26: Ingresa el importe total de la venta")
+  );
+  if (!isNaN(litrosEjercicio26) || !isNaN(importeEjercicio26)) {
+    if (litrosEjercicio26 >= 101 && litrosEjercicio26 <= 300) {
+      document.getElementById("resultados").innerHTML +=
+        "<p>Resultado de Ejercicio 26: El descuento es del 10%. Por lo tanto, su importe es de: AR$</p>" +
+        importeEjercicio26 * 0.1;
+    } else if (litrosEjercicio26 >= 301 && litrosEjercicio26 <= 500) {
+      document.getElementById("resultados").innerHTML +=
+        "<p>Resultado de Ejercicio 26: El descuento es del 15%. Por lo tanto su importe es de: AR$</p>" +
+        importeEjercicio26 * 0.15;
+    } else {
+      document.getElementById("resultados").innerHTML +=
+        "<p>Resultado de Ejercicio 26: No hay descuento, su importe es de: AR$</p>" +
+        importeEjercicio26;
+    }
+  } else {
+    document.getElementById("resultados").innerHTML +=
+      "<p>Resultado de Ejercicio 26: El dato ingresado es incorrecto, por favor ingresa un número. </p>";
+  }
+}
+
+ejercicio26();
 // 5. Hacer un programa que solicite el ingreso de las notas del primer parcial y del
 // segundo parcial de una alumna de programación. El programa deberá analizar
 // las notas y emitir la situación de la alumna según la siguiente escala:
